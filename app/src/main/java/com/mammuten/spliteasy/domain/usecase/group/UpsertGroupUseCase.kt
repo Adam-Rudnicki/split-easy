d337bca1f4682f58f68711c75b0b603f71cca23e
arud <adam.rud02@gmail.com>
2024-01-02 19:01:31 +0100

    basic app

--- app/src/main/java/com/mammuten/spliteasy/domain/usecase/group/UpsertGroupUseCase.kt
+++ app/src/main/java/com/mammuten/spliteasy/domain/usecase/group/UpsertGroupUseCase.kt
@@ -2,7 +2,6 @@ package com.mammuten.spliteasy.domain.usecase.group
 
 import com.mammuten.spliteasy.data.repo.GroupRepo
 import com.mammuten.spliteasy.domain.model.Group
-import com.mammuten.spliteasy.domain.util.InvalidGroupException
 
 class UpsertGroupUseCase(
     private val groupRepo: GroupRepo
--- app/src/main/java/com/mammuten/spliteasy/domain/util/CustomExceptions.kt
+++
@@ -1,3 +0,0 @@
-package com.mammuten.spliteasy.domain.util
-
-class InvalidGroupException(message: String) : Exception(message)
