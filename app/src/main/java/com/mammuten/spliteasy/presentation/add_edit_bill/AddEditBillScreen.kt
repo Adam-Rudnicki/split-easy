efdfa9f78df1383e192ee3476c53c311c4a37577
arud <adam.rud02@gmail.com>
2024-02-19 21:23:19 +0100

    change amount type from Double to Int

--- app/src/main/java/com/mammuten/spliteasy/presentation/add_edit_bill/AddEditBillScreen.kt
+++ app/src/main/java/com/mammuten/spliteasy/presentation/add_edit_bill/AddEditBillScreen.kt
@@ -27,7 +27,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
